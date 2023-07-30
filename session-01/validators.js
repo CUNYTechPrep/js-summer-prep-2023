19c57d131738e83ad1ddd682db3bd11dde18097a
WeiJian123-tech <55195954+WeiJian123-tech@users.noreply.github.com>
2023-07-29 22:18:29 -0400

    Add files via upload
    Completed Homework #1.

--- session-01/validators.js
+++ session-01/validators.js
@@ -7,7 +7,22 @@
   - username cannot contain special characters
 */
 function validUsername(username) {
-  return;
+  const validLength = Boolean(username.length >= 3 && username.length <= 10? true: false);
+  
+  /*
+    /[A-Z]/i: Find if there is an individual character from A to Z going from left to right. 
+    !(/\W/i): Find if there is NOT an individual special character
+    /\D/i: Find if there is NOT an individual number
+    /\w/: Fine a alphanumeric character which is not a special character.
+  */
+
+  const beginningLetter = username.charAt(0);
+  const remainingUsername = username.substring(1, username.length);
+
+  const beginsWithLetter = Boolean(beginningLetter.match(/[A-Z]/i) != null? true: false);
+  const containsLettNum = Boolean(remainingUsername.match(/\w/));
+  const specialCharacter = Boolean(remainingUsername.match(/[\W]/)); 
+  return validLength && beginsWithLetter && containsLettNum && !specialCharacter;
 }
 
 /*
@@ -17,7 +32,11 @@ function validUsername(username) {
   - password must contain at least 1 letter, 1 number, and 1 special character
 */
 function validPassword(password) {
-  return;
+  const validLength = Boolean(password.length >= 10 && password.length <= 64? true: false);
+  const containsLetter = Boolean(password.match(/[A-Z]/i));
+  const containsNumber = Boolean(password.match(/\d/i));
+  const containsSpecialChar = Boolean(password.match(/\W/i));
+  return validLength && containsLetter && containsNumber && containsSpecialChar;
 }
 
 module.exports = { validUsername, validPassword };

5b2a7e107cf16624f648dd87d75b8257fb883dae
JoanG5 <107497667+JoanG5@users.noreply.github.com>
2023-08-02 17:58:47 -0400

    Update validators.js

--- session-01/validators.js
+++ session-01/validators.js
@@ -7,7 +7,18 @@
   - username cannot contain special characters
 */
 function validUsername(username) {
-  return;
+  if (username.length < 3 || username.length > 10){
+    return false
+  }
+  if (!(username[0] >= 'a' && username[0] <= 'z' || username[0] >= 'A' && username[0] <= 'Z')){
+    return false
+  }
+  for (var i = 1; i < username.length; i++){
+    if (!(username[i] >= 'a' && username[i] <= 'z' || username[i] >= 'A' && username[i] <= 'Z' || username[i] >= '0' && username[i] <= '9')){
+      return false
+    }
+  }
+  return true
 }
 
 /*
@@ -17,7 +28,24 @@ function validUsername(username) {
   - password must contain at least 1 letter, 1 number, and 1 special character
 */
 function validPassword(password) {
-  return;
+  is_letter = false
+  is_number = false
+  is_special = false
+  if (password.length < 10 || password.length > 64){
+    return false
+  }
+  for (var i = 0; i < password.length; i++){
+    if (password[i] >= 'a' && password[i] <= 'z' || password[i] >= 'A' && password[i] <= 'Z'){
+      is_letter = true 
+    }
+    else if (password[i] >= '0' && password[i] <= '9'){
+      is_number = true 
+    }
+    else{
+      is_special = true
+    }
+  }
+  return is_letter && is_number && is_special
 }
 
 module.exports = { validUsername, validPassword };

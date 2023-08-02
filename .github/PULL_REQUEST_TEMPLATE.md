7eebba3450789a5ee77c14a4fcf77e73f0484ad1
MystProgrammer <animeshdeb89@gmail.com>
2023-08-01 21:17:02 -0400

    Complete validators.js

--- .github/PULL_REQUEST_TEMPLATE.md
+++ .github/PULL_REQUEST_TEMPLATE.md
@@ -8,12 +8,12 @@
 
 **Is the solution complete?**
 
-[Yes/No]
+[Yes]
 
 **Did you run into any problems?**
 
-[Describe...]
+[I guess the main problem I had was not knowing how to test for special characters, because these special characters are characters that are not a-z, A-Z, and 0-9. I didn't know the syntax of regex that would allow me to do that. I was trying to use the ! since I was used to using this symbol to negate inequalities and similar notations. However, doing /![ a-zA-Z0-9]/ didn't work. Accordingly, in order to understand how regex worked fully, I decided to research online on how to use regex expressions. This led me to see that you can use the ^ symbol to negate things in a regular expression. As such, I leared that you can use /[ ^a-zA-Z0-9]/ to target special characters.
 
 **Did you collaborate with others on this solution?**
 
-<!-- Provide github usernames -->
+No, I didn't collaborate with anyone.
--- session-01/validators.js
+++ session-01/validators.js
@@ -1,23 +1,48 @@
 /*
   Write a function that returns true or false if the given username
   is valid according to the following rules:
-  - username must be between (and including) 3-10 characters in length
-  - username must begin with a letter
-  - username may contain numbers and letters
-  - username cannot contain special characters
+  - username must be between (and including) 3-10 characters in length- done
+  - username must begin with a letter-done-done
+  - username may contain numbers and letters-done
+  - username cannot contain special characters-done
 */
+
 function validUsername(username) {
-  return;
+  let isUsernameValid = false;
+  if (/[^a-zA-z0-9]/.test(username)) {
+    isUsernameValid = false;
+    return isUsernameValid;
+  } else {
+    if (username.length >= 3 && username.length <= 10) {
+      if (/[a-zA-Z0-9]/.test(username)) {
+        if (/[a-zA-Z]/.test(username[0])) {
+          isUsernameValid = true;
+        }
+      }
+    }
+  }
+
+  return isUsernameValid;
 }
 
 /*
   Write a function that returns true or false if the given password
   is valid according to the following rules:
-  - password must be between (and including) 10-64 characters in length
-  - password must contain at least 1 letter, 1 number, and 1 special character
+  - password must be between (and including) 10-64 characters in length-done
+  - password must contain at least 1 letter, 1 number, and 1 special character-done
 */
 function validPassword(password) {
-  return;
+  let isPasswordValid = false;
+  if (password.length >= 10 && password.length <= 64) {
+    if (/[a-zA-Z]/.test(password)) {
+      if (/[0-9]/.test(password)) {
+        if (/[^a-zA-Z0-9]/.test(password)) {
+          isPasswordValid = true;
+        }
+      }
+    }
+    return isPasswordValid;
+  }
 }
 
 module.exports = { validUsername, validPassword };

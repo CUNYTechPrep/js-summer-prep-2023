c58e3b598b42fc0b7c6eb3bd03bf7592ce0e1fc0
Golam Rabbani <rabbanimoon1410@gmail.com>
2023-08-02 16:09:52 -0400

    updating all my files

--- session-01/validators.js
+++ session-01/validators.js
@@ -6,8 +6,26 @@
   - username may contain numbers and letters
   - username cannot contain special characters
 */
+
+
 function validUsername(username) {
-  return;
+   //Checking if the username is between (and including) 3-10 characters in length
+  if (username.length < 3 || username.length > 10) {
+    return false;
+  }
+
+  // Checking if the first character is a letter using regular expression
+  if (!username[0].match(/[a-zA-Z]/)) {
+    return false;
+  }
+
+  // Checking if the username contains only numbers and letters using regular expression
+  if (!username.match(/^[a-zA-Z0-9]+$/)) {
+    return false;
+  }
+
+  // If all checks pass, returning true (valid username)
+  return true;
 }
 
 /*
@@ -16,8 +34,20 @@ function validUsername(username) {
   - password must be between (and including) 10-64 characters in length
   - password must contain at least 1 letter, 1 number, and 1 special character
 */
+
+
 function validPassword(password) {
-  return;
+  // Checking if the password is between (and including) 10-64 characters in length
+  if (password.length < 10 || password.length > 64) {
+    return false;
+  }
+  // Checking if the password contain at least 1 letter, 1 number, and 1 special character
+  if (!password.match(/[a-zA-Z]/) || !password.match(/\d/) || !password.match(/[!@#$%^&*()_+{}\[\]:;<>,.?~\\|]/)) {
+    return false;
+  }
+
+  return true;
 }
+
 
 module.exports = { validUsername, validPassword };

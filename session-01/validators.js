33b9aee6faa18607ef9c5bb07d86723c9501506d
zyleq <thompsd11@gmail.com>
2023-08-03 19:07:04 -0400

    finished assigned

--- session-01/validators.js
+++ session-01/validators.js
@@ -7,7 +7,20 @@
   - username cannot contain special characters
 */
 function validUsername(username) {
-  return;
+  if (username.length < 3 || username.length > 10) {
+    return false;
+  }
+  if (!(username.charAt(0).toLowerCase() >= 'a' && username.charAt(0).toLowerCase() <= 'z')) {
+    return false;
+  }
+  for (let i = 0; i < username.length; i++) {
+    let character = username.charAt(i);
+    if (!(character.toLowerCase() >= 'a' && character.toLowerCase() <= 'z') &&
+        !(character >= '0' && character <= '9')) {
+      return false;
+    }
+  }
+  return true;
 }
 
 /*
@@ -17,7 +30,37 @@ function validUsername(username) {
   - password must contain at least 1 letter, 1 number, and 1 special character
 */
 function validPassword(password) {
-  return;
+  if (password.length < 10 || password.length > 64) {
+    return false;
+  }
+
+  let hasLetter = false;
+  let hasNumber = false;
+  let hasSpecialChar = false;
+  let specialChars = "!@#$%^&*()_+=-[]{};:'\"\\|,.<>?/";
+
+  for (let i = 0; i < password.length; i++) {
+    let character = password.charAt(i);
+
+    if ((character >= 'a' && character <= 'z') || (character >= 'A' && character <= 'Z')) {
+      hasLetter = true;
+    }
+
+    else if (character >= '0' && character <= '9') {
+      hasNumber = true;
+    }
+
+    else if (specialChars.indexOf(character) !== -1) {
+      hasSpecialChar = true;
+    }
+
+    else {
+      return false;
+    }
+  }
+
+  return hasLetter && hasNumber && hasSpecialChar;
 }
+
 
 module.exports = { validUsername, validPassword };

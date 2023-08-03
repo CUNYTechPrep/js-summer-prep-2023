e1db74a971acdd2d8f8b870d40df2e2b7d26d9f6
darcierre <darrnurse@gmail.com>
2023-08-03 17:56:01 -0400

    Is the solution complete?
    Yes
    
    Did you run into any problems?
    
    I had to learn a lot of Git just now! It was hard to understand npm and get it to test,
    
    Did you collaborate with others on this solution?
    No

--- session-01/validators.js
+++ session-01/validators.js
@@ -7,7 +7,37 @@
   - username cannot contain special characters
 */
 function validUsername(username) {
-  return;
+  let number_array = ["0","1","2","3","4","5","6","7","8","9"]; //these are the only 10 numbers (as strings) that could occur as a first character
+  let symbol_array = ["!","@","#","$","%","^","&","*","(",")",".",",","?"]; //'special characters' may not be defined the same way in each system, array can be changed to changed definition in function
+
+  let username_is_valid= false; //inner functions only change these variables to booleans if they are true so initialize them as booleans
+
+  let length_check = false;
+  let first_letter_check = false;
+  let symbol_check = false;
+
+  if(username.length >= 3 && username.length <= 10){
+    length_check = true;
+  }
+
+  if(!(number_array.includes(username[0]))){ //once it is determined that it does begin with a letter, then "may contain letters and numbers" is satisfied
+    first_letter_check = true;
+  }
+
+  for (let i = 0; i < username.length; i++) {
+    if(symbol_array.includes(username[i])){
+      symbol_check = false;
+      break;
+    } else {
+      symbol_check = true;
+    }
+  }
+
+  if(length_check && first_letter_check && symbol_check){
+    username_is_valid = true;
+  }
+
+  return username_is_valid;
 }
 
 /*
@@ -17,7 +47,44 @@ function validUsername(username) {
   - password must contain at least 1 letter, 1 number, and 1 special character
 */
 function validPassword(password) {
-  return;
+
+  let number_array = ["0","1","2","3","4","5","6","7","8","9"];
+  let symbol_array = ["!","@","#","$","%","^","&","*","(",")"];
+
+  let password_is_valid = false;
+
+  let length_check = false;
+  let all_char_check = false;
+
+  if(password.length >= 10 && password.length <= 64){
+    length_check = true;
+  }
+
+  let letter_check = false;
+  let number_check = false;
+  let symbol_check = false;
+
+  for (let i = 0; i < password.length; i++){
+    if(symbol_array.includes(password[i])){
+      symbol_check = true;
+    }
+    if(number_array.includes(password[i])){
+      number_check = true;
+    }
+    if(!symbol_array.includes(password[i]) && !number_array.includes(password[i])){
+      letter_check = true;
+    }
+    if(symbol_check && number_check && letter_check){
+      all_char_check = true;
+      break;
+    }
+  }
+
+  if(length_check && all_char_check){
+    password_is_valid = true;
+  }
+
+  return password_is_valid;
 }
 
 module.exports = { validUsername, validPassword };

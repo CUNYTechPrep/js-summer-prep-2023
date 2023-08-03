20f0c7c8c9a8ac3d7050c439c74d288f19783766
darcierre <darrnurse@gmail.com>
2023-08-03 18:45:47 -0400

    Updated symbols array.

--- session-01/validators.js
+++ session-01/validators.js
@@ -49,7 +49,7 @@ function validUsername(username) {
 function validPassword(password) {
 
   let number_array = ["0","1","2","3","4","5","6","7","8","9"];
-  let symbol_array = ["!","@","#","$","%","^","&","*","(",")"];
+  let symbol_array = ["!","@","#","$","%","^","&","*","(",")",".",",","?"];
 
   let password_is_valid = false;
 

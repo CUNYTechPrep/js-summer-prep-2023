8d4a36cf649af0cc6d04e12a333b49d6f7681568
Daniel Samborski <abracadaniel64@gmail.com>
2023-08-03 17:58:48 -0400

    homework 1

--- session-01/validators.js
+++ session-01/validators.js
@@ -1,13 +1,35 @@
 /*
   Write a function that returns true or false if the given username
   is valid according to the following rules:
-  - username must be between (and including) 3-10 characters in length
-  - username must begin with a letter
+  - username must be between (and including) 3-10 characters in length done
+  - username must begin with a letter done
   - username may contain numbers and letters
   - username cannot contain special characters
 */
 function validUsername(username) {
-  return;
+  
+  if (username.length < 3 || username.length > 10){
+    return false;
+  }
+  
+  let firstCharacter = username[0].toLowerCase();
+  if (!(firstCharacter >= 'a' && firstCharacter <= 'z')){
+    return false;
+  }
+
+  const alphanumeric = /^[a-zA-Z0-9]+$/
+  if(!alphanumeric.test(username)){
+    return false;
+  }
+
+  const specialChar = /[ `!@#$%^&*()_+\-=\[\]{};':"\\|,.<>\/?~]/
+  if(specialChar.test(username)){
+    return false;
+  }
+  
+  return true;
+
+
 }
 
 /*
@@ -17,7 +39,21 @@ function validUsername(username) {
   - password must contain at least 1 letter, 1 number, and 1 special character
 */
 function validPassword(password) {
-  return;
+
+  if (!(password.length >= 10 && password.length <= 64)){
+    return false;
+  }
+
+
+  const letter = /[a-zA-Z]/
+  const number = /\d/
+  const specialChar = /[ `!@#$%^&*()_+\-=\[\]{};':"\\|,.<>\/?~]/
+
+  if(!letter.test(password) || !number.test(password) || !specialChar.test(password)){
+    return false;
+  }
+
+  return true;
 }
 
 module.exports = { validUsername, validPassword };

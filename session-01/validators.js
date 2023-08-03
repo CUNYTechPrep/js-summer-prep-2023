df9ddd65e3a670ef9809d92fa14a4a9fec4352d7
csi-Abil <abil.shrestha@cix.csi.cuny.edu>
2023-08-03 16:33:28 -0400

    h1 all test passed

--- session-01/validators.js
+++ session-01/validators.js
@@ -6,20 +6,28 @@
   - username may contain numbers and letters
   - username cannot contain special characters
 */
+function checkString(str, type) {
+  switch(type) {
+    case "Letters":
+      return /[a-zA-Z]/.test(str)
+    case "Specials":
+      return /[`!@#$%^&*()_+\-=\[\]{};':"\\|,.<>\/?~]/.test(str)
+    case "Numbers":
+      return /[0-9]/.test(str)
+    default:
+      return false;
+  }
+}
+
 function validUsername(username) {
   if (username.length < 3 || username.length > 10) {
     return false;
-  }
-
-  // Check if the first character of the username is a letter
-  if (!/^[A-Za-z]/.test(username)) {
+  } else if (checkString(username[0], "Letters") === false) {
+    return false;
+  } else if (checkString(username, "Specials")) {
     return false;
   }
-
-  // Check if the username contains only letters and numbers
-  if (!/^[A-Za-z0-9]+$/.test(username)) {
-    return false;
-  }
+  return true;
 }
 
 /*
@@ -32,12 +40,9 @@ function validPassword(password) {
   if (password.length < 10 || password.length > 64) {
     return false;
   }
-
-  // Check if the password contains at least one letter, one number, and one special character
   if (!/[A-Za-z]/.test(password) || !/\d/.test(password) || !/[!@#$%^&*()_+{}\[\]:;<>,.?~\\\-|=]/.test(password)) {
     return false;
   }
-
   return true;
 }
 

5c01c0cbce7f83282e4dfe8958b222ecfd080b40
Puja Roy <pujaroy280@gmail.com>
2023-08-04 23:33:42 -0400

    Completed exercises for session-02

--- session-02/exercise.js
+++ session-02/exercise.js
@@ -2,9 +2,29 @@
   Transform the input array of strings into uppercase strings
   For example, for the input ["cat", "hat"], return ["CAT", "HAT"]
 */
+
+// Function to transform the input array of strings into uppercase strings
 function transformArrayToUpper(listOfStrings) {
-  return;
+  // Create an empty array to store the uppercase strings
+  let resultArray = [];
+
+  // Loop through each string in the input array
+  for (let i = 0; i < listOfStrings.length; i++) {
+    // Convert the current string to uppercase using the toUpperCase() method
+    let uppercaseString = listOfStrings[i].toUpperCase();
+
+    // Add the uppercase string to the result array
+    resultArray.push(uppercaseString);
+  }
+
+  // Return the array with all strings converted to uppercase
+  return resultArray;
 }
+
+// Test the function with example input
+const inputArray = ["cat", "hat"];
+const result = transformArrayToUpper(inputArray);
+console.log(result); // Output: ["CAT", "HAT"]
 
 /*
   Write a function that returns the sum of all student ages.
@@ -15,8 +35,34 @@ function transformArrayToUpper(listOfStrings) {
     [{ name: 'Sandra', age: 31 }, {}, { name: 'Didi', age: 20}]
     the function should return 51
 */
+
+// Function to calculate the sum of all student ages
 function sumOfAllAges(listOfStudentObjects) {
-  return;
+  // Initialize a variable to store the sum of ages
+  let sum = 0;
+
+  // Loop through each student object in the input array
+  for (let i = 0; i < listOfStudentObjects.length; i++) {
+    // Check if the current object has an "age" property and it is a number
+    if (typeof listOfStudentObjects[i].age === "number") {
+      // If the "age" property exists and is a number, add it to the sum
+      sum += listOfStudentObjects[i].age;
+    }
+  }
+
+  // Return the total sum of ages
+  return sum;
 }
+
+// Test the function with example input
+const students = [
+  { name: 'Sandra', age: 31 },
+  {},
+  { name: 'Didi', age: 20 }
+];
+
+const totalAge = sumOfAllAges(students);
+console.log(totalAge); // Output: 51
+
 
 module.exports = { transformArrayToUpper, sumOfAllAges };

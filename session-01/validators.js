c3c0fcc639122db595414a09139536fcdca6c895
ramyghoneim <ramymghoneim@gmail.com>
2023-08-07 16:43:41 -0400

    completion of assignment 1

--- session-01/validators.js
+++ session-01/validators.js
@@ -1,23 +1,20 @@
-/*
-  Write a function that returns true or false if the given username
-  is valid according to the following rules:
-  - username must be between (and including) 3-10 characters in length
-  - username must begin with a letter
-  - username may contain numbers and letters
-  - username cannot contain special characters
-*/
 function validUsername(username) {
-  return;
+  if ((username.length > 2 && username.length < 11) && ((/[a-zA-Z]/).test(username.charAt(0))) &&
+    !((/[`!@#$%^&*()_\-+=\[\]{};':"\\|,.<>\/?~ ]/).test(username))) {
+    return true
+  }
+  else {
+    return false
+  }
 }
-
-/*
-  Write a function that returns true or false if the given password
-  is valid according to the following rules:
-  - password must be between (and including) 10-64 characters in length
-  - password must contain at least 1 letter, 1 number, and 1 special character
-*/
 function validPassword(password) {
-  return;
+  if ((password.length > 9 && password.length < 65) && (((/[a-zA-Z]/).test(password)) && (/\d/).test(password)) &&
+    ((/[`!@#$%^&*()_\-+=\[\]{};':"\\|,.<>\/?~ ]/).test(password))) {
+    return true
+  }
+  else {
+    return false
+  }
 }
 
 module.exports = { validUsername, validPassword };

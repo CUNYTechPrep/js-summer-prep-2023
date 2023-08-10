ee6423169c681f3d5481d92705f5514d13529cff
Joseph <inumoj@gmail.com>
2023-08-10 13:16:11 -0400

    Adding solution for HW1

--- session-01/validators.js
+++ session-01/validators.js
@@ -7,7 +7,11 @@
   - username cannot contain special characters
 */
 function validUsername(username) {
-  return;
+    //the variable pattern checks 
+        //first if the first character is a letter [a-zA-Z] 
+        //then the characters can be any letter or number [/w/d]
+        var pattern = new RegExp(/^[a-zA-Z][\w\d]{2,9}$/);
+        return pattern.test(username); // then the test ,method of the pattern class is used
 }
 
 /*
@@ -17,7 +21,14 @@ function validUsername(username) {
   - password must contain at least 1 letter, 1 number, and 1 special character
 */
 function validPassword(password) {
-  return;
+
+  //the variable pattern checks 
+        //using look ariund if the password has a letter /(?=.*[\w]
+        // or a number (?=.*[\d]
+        //or any of these special characters (?=.*[!@#$%^&*()\-_=+{};:,<.>]).*$/);
+        var pattern = new RegExp(/(?=.*[\w])(?=.*[\d])(?=.*[!@#$%^&*()\-_=+{};:,<.>]).*$/);
+         return pattern.test(password);  // then the test method of the pattern class is used
+         
 }
 
 module.exports = { validUsername, validPassword };

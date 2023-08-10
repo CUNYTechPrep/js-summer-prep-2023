a46135f9b97b24b2c456303cd96a065fe0209f4a
lolfjp <fujunp9@gmail.com>
2023-08-10 16:21:30 -0400

    validator.js solution

--- session-01/validators.js
+++ session-01/validators.js
@@ -7,16 +7,22 @@
   - username cannot contain special characters
 */
 function validUsername(username) {
-    
-    //username 3-10 characters
-    if (username.length < 3 || username.length > 10) {
-      return false;
-    }
-    //username start with letter
-    if (!"^") {
-      return false;
-    }
-    
+  if (username.length < 3 || username.length > 10) return false;
+
+  if (
+    !(username[0] >= "a" && username[0] <= "z") &&
+    !(username[0] >= "A" && username[0] <= "Z")
+  )
+    return false;
+
+  for (let i = 1; i < username.length; i++) {
+    let letter = username[i];
+    if (letter >= "a" && letter <= "z") continue;
+    if (letter >= "A" && letter <= "Z") continue;
+    if (letter >= "0" && letter <= "9") continue;
+    return false;
+  }
+
   return true;
 }
 
@@ -27,24 +33,32 @@ function validUsername(username) {
   - password must contain at least 1 letter, 1 number, and 1 special character
 */
 function validPassword(password) {
+  if (password.length < 10 || password.length > 64) return false;
 
-  //password 10-64 characters
-  if(password.length >= 10 && password.length <= 64) {
-    //password contain atleast 1 letter, 1 number, 1 special character
-    if(!(/[a-zA-z]/.test(password))) {
-      return false;
-    }
-    if(!(/[0-9]/.test(password))) {
-      return false;
-    }
-    return true;
-    
+  let numberCount = 0;
+  for (let i = 0; i < password.length; i++) {
+    let letter = password[i];
+    if (letter >= "0" && letter <= "9") numberCount++;
   }
-    else {
-    return false;
+  if (numberCount === 0) return false;
 
+  let alphaCount = 0;
+  for (let i = 0; i < password.length; i++) {
+    let letter = password[i];
+    if ((letter >= "a" && letter <= "z") || (letter >= "A" && letter <= "Z"))
+      alphaCount++;
   }
+  if (alphaCount === 0) return false;
 
+  const specialPrintableCharacters = " !\"#$%&'()*+,-./:;<=>?@{\\]^_`{|}~";
+  let specialCount = 0;
+  for (let i = 0; i < password.length; i++) {
+    let letter = password[i];
+    if (specialPrintableCharacters.includes(letter)) specialCount++;
+  }
+  if (specialCount === 0) return false;
+
+  return true;
 }
 
 module.exports = { validUsername, validPassword };

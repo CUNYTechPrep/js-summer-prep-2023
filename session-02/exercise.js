000b408d896452a1fff66bad25c40b3f6ef4bfc6
Pablo Lara <pablolara01@yahoo.com>
2023-08-09 23:25:09 -0400

    Update with solutions for homework 2.

--- session-02/exercise.js
+++ session-02/exercise.js
@@ -15,7 +15,7 @@ function transformArrayToUpper(listOfStrings) {
 /*
   Write a function that returns the sum of all student ages.
   The function will be passed an array of objects and the result will be the sum of all ages.
-  - Note, not all objects will contain an age. Omit these objects.
+  - NOTE: Not all objects will contain an age. Omit these objects.
   For example, for the input:
     [{ name: 'Sandra', age: 31 }, {}, { name: 'Didi', age: 20}]
     the function should return 51

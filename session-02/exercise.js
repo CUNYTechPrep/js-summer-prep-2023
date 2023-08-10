c13477f6eecbaea830e19c4395a556621e6176eb
JinLin6398 <jinlin11220@gmail.com>
2023-08-09 22:15:12 -0400

    week3hw

--- session-02/exercise.js
+++ session-02/exercise.js
@@ -3,7 +3,13 @@
   For example, for the input ["cat", "hat"], return ["CAT", "HAT"]
 */
 function transformArrayToUpper(listOfStrings) {
-  return;
+    // Iterate through each string in the array
+    for (let i = 0; i < listOfStrings.length; i++) {
+        // Convert the current string to uppercase and update the array element
+        listOfStrings[i] = listOfStrings[i].toUpperCase();
+    }
+    // Return the modified array of uppercase strings
+    return listOfStrings;
 }
 
 /*
@@ -16,7 +22,18 @@ function transformArrayToUpper(listOfStrings) {
     the function should return 51
 */
 function sumOfAllAges(listOfStudentObjects) {
-  return;
+  // Initialize the sum to keep track of the total age
+  let sum = 0;
+  // Iterate through each object in the array
+  for (let i = 0; i < listOfStudentObjects.length; i++) {
+    // Check if the current object has a numeric age property
+    if (typeof listOfStudentObjects[i].age === 'number') {
+      // Add the age of the current object to the sum
+      sum += listOfStudentObjects[i].age;
+    }
+  }
+  // Return the calculated sum of ages
+  return sum;
 }
 
 module.exports = { transformArrayToUpper, sumOfAllAges };

55c4e1ce726bc9540ce4a36436d6ee98431a419c
matteoSaputo <matteosaputo1201@gmail.com>
2023-08-10 15:29:55 -0400

    Completed Assignment

--- session-02/exercise.js
+++ session-02/exercise.js
@@ -3,7 +3,10 @@
   For example, for the input ["cat", "hat"], return ["CAT", "HAT"]
 */
 function transformArrayToUpper(listOfStrings) {
-  return;
+  for(let i = 0; i < listOfStrings.length; i++){
+    listOfStrings[i] = listOfStrings[i].toUpperCase();
+  }
+  return listOfStrings;
 }
 
 /*
@@ -16,7 +19,13 @@ function transformArrayToUpper(listOfStrings) {
     the function should return 51
 */
 function sumOfAllAges(listOfStudentObjects) {
-  return;
+  let sum = 0;
+  for(const student of listOfStudentObjects){
+    if(student.age)
+      sum += student.age;
+  }
+  return sum;
 }
+
 
 module.exports = { transformArrayToUpper, sumOfAllAges };

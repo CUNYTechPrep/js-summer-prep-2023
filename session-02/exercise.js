53b70c46d52d19b4fc783cb84cfefe0f260858a5
zyleq <thompsd11@gmail.com>
2023-08-10 17:36:06 -0400

    Davion HW2 submission

---
+++ session-02/exercise.js
@@ -0,0 +1,29 @@
+/*
+  Transform the input array of strings into uppercase strings
+  For example, for the input ["cat", "hat"], return ["CAT", "HAT"]
+*/
+function transformArrayToUpper(listOfStrings) {
+  listOfStrings.forEach((listOfString, index, arr) => {
+    arr[index] = listOfString.toUpperCase();
+  });
+  return listOfStrings;
+}
+
+/*
+  Write a function that returns the sum of all student ages.
+  The function will be passed an array of objects and the result
+  will be the sum of all ages.
+  - Note, not all objects will contain an age. Omit these objects.
+  For example, for the input:
+    [{ name: 'Sandra', age: 31 }, {}, { name: 'Didi', age: 20}]
+    the function should return 51
+*/
+function sumOfAllAges(listOfStudentObjects) {
+  let sum = 0;
+  listOfStudentObjects.forEach(listOfStudentObject => { 
+    if(!isNaN(listOfStudentObject.age)){sum += listOfStudentObject.age;
+  }});
+  return sum;
+}
+
+module.exports = { transformArrayToUpper, sumOfAllAges };

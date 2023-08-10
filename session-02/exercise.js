a78405be5a65fa04a8e6b579de52d41c57841595
ramyghoneim <ramymghoneim@gmail.com>
2023-08-10 17:05:58 -0400

    completion of assignment 2

--- session-02/exercise.js
+++ session-02/exercise.js
@@ -2,10 +2,15 @@
   Transform the input array of strings into uppercase strings
   For example, for the input ["cat", "hat"], return ["CAT", "HAT"]
 */
-function transformArrayToUpper(listOfStrings) {
-  return;
-}
 
+function transformArrayToUpper(listOfStrings) {
+  let upperCaseArray = new Array()
+
+  for (let x = 0; x < listOfStrings.length; x++){
+    upperCaseArray[x] = listOfStrings[x].toUpperCase()
+  }
+  return upperCaseArray
+}
 /*
   Write a function that returns the sum of all student ages.
   The function will be passed an array of objects and the result
@@ -16,7 +21,16 @@ function transformArrayToUpper(listOfStrings) {
     the function should return 51
 */
 function sumOfAllAges(listOfStudentObjects) {
-  return;
+  let cumulativeAges = 0
+  let currentAge
+
+  for (let x = 0; x < listOfStudentObjects.length; x++) {
+    if (listOfStudentObjects[x].hasOwnProperty("age")) {
+      currentAge = listOfStudentObjects[x].age
+      cumulativeAges = cumulativeAges + currentAge
+    }
+  }
+  return cumulativeAges
 }
 
 module.exports = { transformArrayToUpper, sumOfAllAges };
